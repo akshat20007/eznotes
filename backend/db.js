71eddaddd3d3243127aaecd6417ebb0b77eb2839
Akshat Bahukhandi <akshat.b2007@gmail.com>
2021-11-20 22:14:36 +0530

    validation added

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
+const mongoURI = 'mongodb://localhost:27017/ezNotes?readPreference=primary&appname=MongoDB%20Compass&ssl=false'
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,23 @@
  const express = require('express');
  const router = express.Router();
  const User = require('../models/user');
+ const { body, validationResult } = require('express-validator');
+
 
 //create a user using: POST "/api/auth/". Doesn't require auth
- router.post('/', (req,res)=>{
+ router.post('/',[body('email','Enter a valid email').isEmail(),body('name','Enter a valid name').isLength({ min: 5 }),  body('password','Enter a valid password').isLength({ min: 5 })], (req,res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      }).then(user => res.json(user))
+      .catch(err=>console.log(err));
      
-     console.log(req.body);
-     const user = User(req.body);
-     user.save();
-     res.send(req.body);
- })
+     
+ }) 
 
  module.exports = router;
